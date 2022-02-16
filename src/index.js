c0a1436e171e706aa3634461e7d14ea5d800a4a4
Diego Felix de Souza <83612963+Diego-Felix@users.noreply.github.com>
2022-02-16 15:11:51 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Potato</li>
+      <li>Rice</li>
+      <li>Pasta</li>
+    </ul>
+    <ol>
+      <li>Ice cream</li>
+      <li>Candy</li>
+      <li>Orange Juice</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
